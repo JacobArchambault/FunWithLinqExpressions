46b6dd049212b8be3bed998b7c87a9323428df04
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-04 13:14:56 -0400

    Correct spelling error

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace FunWithLinqExpressions
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("***** Fun with Query Expressions *****");
 
@@ -16,7 +16,7 @@ namespace FunWithLinqExpressions
                 new ProductInfo{Name = "Mac's Coffee", Description = "Coffee with TEETH", NumberInStock = 24 },
                 new ProductInfo{Name = "Milk Maid Milk", Description = "Milk cow's love", NumberInStock = 100 },
                 new ProductInfo{Name = "Pure Silk Tofu", Description = "Bland as Possible", NumberInStock = 120 },
-                new ProductInfo{Name = "Cruncy Pops", Description = "Cheezy, peppery goodness", NumberInStock = 2 },
+                new ProductInfo{Name = "Crunchy Pops", Description = "Cheezy, peppery goodness", NumberInStock = 2 },
                 new ProductInfo{Name = "RipOff Water", Description = "From the tap to your wallet", NumberInStock = 100 },
                 new ProductInfo{Name = "Classic Valpo Pizza", Description = "Everyone loves pizza!", NumberInStock = 73 },
             };
